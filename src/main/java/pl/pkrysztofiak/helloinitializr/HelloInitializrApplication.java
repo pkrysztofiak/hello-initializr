a0fef301abfa780e55a41bace11349651e611a38
Przemek Krysztofiak <p.krysztofiak@protonmail.com>
2022-02-05 06:40:49 +0100

    Add maven nature Closes #1

---
+++ src/main/java/pl/pkrysztofiak/helloinitializr/HelloInitializrApplication.java
@@ -0,0 +1,13 @@
+package pl.pkrysztofiak.helloinitializr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloInitializrApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HelloInitializrApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/pkrysztofiak/helloinitializr/HelloInitializrApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.pkrysztofiak.helloinitializr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloInitializrApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
